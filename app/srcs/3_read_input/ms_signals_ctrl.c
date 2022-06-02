60a7118b88417f9d40955a1ab58cee19a4612cf4
argelcapela <argelcapeladossantos@gmail.com>
2022-06-02 01:14:25 -0300

    fixed ctrl + C redisplay

--- app/srcs/3_read_input/ms_signals_ctrl.c
+++ app/srcs/3_read_input/ms_signals_ctrl.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_signals_ctrl.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acapela- <acapela-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: argel <argel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 20:37:54 by acapela-          #+#    #+#             */
-/*   Updated: 2022/05/31 21:47:29 by acapela-         ###   ########.fr       */
+/*   Updated: 2022/06/02 01:13:52 by argel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ void	ctrl_c_redisplay_prompt(int signal)
 {
 	(void)signal;
 	ft_printf("\n");
-	ms_print_sh_name();
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
 }
 
 void	ctrl_c_interrupt_process(int signal)
